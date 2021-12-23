c367b1ff1a2f5cc32e2a3e7c1a85e005115368d9
Fersan <fst-96@hotmail.com>
2021-12-22 19:46:00 -0600

    Division entre dos numero y entero

---
+++ 01-Divide-two-integers.ts
@@ -0,0 +1,8 @@
+function divide(dividend: number, divisor: number): number {
+    if(dividend === -2147483648 && divisor === -1){
+        return 2147483647;
+    }
+    return Math.trunc(dividend/divisor);
+};
+
+console.log(divide(10,3));   
