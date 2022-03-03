bc704e5ca2d1a402c996462ffb8161889ad83c5d
fernando-ui <fst-96@hotmail.com>
2022-03-02 20:21:30 -0600

    Ejercicio numero 2 con for

--- 02-add-two-numbers.ts
+++ 02-add-two-numbers.ts
@@ -1,7 +1,16 @@
+  class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+      }
+  }
+
 const addTwoNumber = (l1:number[] | null, l2: number[] | null):number[]| null => {
     let l1Reverse = (l1) ? l1.reverse():[];
     let l2Reverse = (l2) ? l2.reverse():[];
-    // TODO aplicar un reverse con for
+  
 
     let respUniendo1 = l1Reverse.join('');
     let respUniendo2 = l2Reverse.join('');
@@ -12,16 +21,41 @@ const addTwoNumber = (l1:number[] | null, l2: number[] | null):number[]| null =>
     let respTotalArr = ansString.split('');
 
     let respNum = respTotalArr.map((value) => +value);
-    // TODO aplicar un reverse con for
+    
     let algo = respNum.reverse()
     return algo;
 }
-console.log(addTwoNumber([0], [1]));
+// console.log(addTwoNumber([1,4,0], [1,3,5]));
 
-const reverso = (l1:number[] | null, l2: number[] | null):number[] => {
+const reverso = (l1:number[] | null, l2: number[] | null):number[] | null => {
+    let algomas = new ListNode()
+     l1 = l1 ? l1 : [];
+     l2 = l2 ? l2 : [];
 
+    let arr1:number[] = [];
+    let arr2:number[] = [];
+    let algo:number[] = [];
+    for(let i = l1.length - 1; i >= 0; i--  ) { 
+        arr1.push(l1[i]);
+    }
+    for(let i = l2.length - 1; i >= 0; i--  ) { 
+        arr2.push(l2[i]);
+    }
     
-    return [3]
+    let respUniendo1 = arr1.join('');
+    let respUniendo2 = arr2.join('');
+    
+    let ansSum = +respUniendo1 + +respUniendo2;
+    let ansString = ansSum + '';
+    let respTotalArr = ansString.split('');
 
+    let respNum = respTotalArr.map((value) => +value);
+    for(let i = respNum.length - 1; i >= 0; i--  ) { 
+        algo.push(respNum[i]);
+    }
+    console.log(typeof algo);
+    return algo;
 }
+
+
 
