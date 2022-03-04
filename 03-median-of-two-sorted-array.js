25f800ea7266bf5b6da695ceeeb4ba1c4c00e873
fernando-ui <fst-96@hotmail.com>
2022-03-03 20:33:28 -0600

    acomodando problema 03-median de leetcode

--- 03-median-of-two-sorted-array.js
+++ 03-median-of-two-sorted-array.js
@@ -13,7 +13,8 @@ function findMedianSortedArrays(nums1, nums2) {
         if (+arrayOrdenado[arrayOrdenado.length - 1] === 0 || +arrayOrdenado[0] === -1) {
             return 0;
         }
-        par = arrayOrdenado.length / 2;
+        par = +arrayOrdenado[arrayOrdenado.length - 1] / 2;
+        console.log(+arrayOrdenado[arrayOrdenado.length - 1], 'El numero');
         parPlus = par + 1;
         resultadoPar = (par + parPlus) / 2;
         return resultadoPar;
@@ -25,12 +26,13 @@ function findMedianSortedArrays(nums1, nums2) {
         if (+arrayOrdenado[arrayOrdenado.length - 1] === 0 || +arrayOrdenado[0] === -1) {
             return 0;
         }
-        impar = arrayOrdenado.length / 2;
+        impar = +arrayOrdenado[arrayOrdenado.length] / 2;
         impar = Math.ceil(impar);
+        console.log('impar');
         return impar;
     }
 }
 ;
-console.log(findMedianSortedArrays([0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 1]));
+console.log(findMedianSortedArrays([], [2, 3]));
 const numeros = [1, 3, 0];
 let respuesta = numeros.sort(function (a, b) { return a - b; });
--- 03-median-of-two-sorted-array.ts
+++ 03-median-of-two-sorted-array.ts
@@ -1,43 +1,38 @@
-function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
+  const arrCompleto: object[] = [];
+  arrCompleto.push(nums1, nums2);
 
-    let arrCompleto:object[] = [];
-    arrCompleto.push(nums1,nums2);
+  let arrayOrdenado = arrCompleto.flat();
+  arrayOrdenado = arrayOrdenado.sort(function (a: any, b: any) {
+    return a - b;
+  });
 
-    let arrayOrdenado = arrCompleto.flat() ;
-    arrayOrdenado = arrayOrdenado.sort(function(a:any, b:any){return a - b});
-    console.log(arrayOrdenado);
+  let par: number | null = null;
+  let parPlus: number | null = null;
+  let resultadoPar: number | null = null;
 
+  let impar: number | null = null;
+  let imparPosicion: number | null = null;
 
-    let par:number | null = null;
-    let parPlus:number | null = null;
-    let resultadoPar:number | null = null;
-    let impar:number | null = null;
+  if (arrayOrdenado.length % 2 === 0) {
+    par = (arrayOrdenado.length - 1) / 2;
 
+    par = Math.floor(par);
+    let parPosicion = par;
+    par = +arrayOrdenado[par];
 
-    if(arrayOrdenado.length %2 === 0){
-        if(+arrayOrdenado[arrayOrdenado.length - 1] === 0 ||  +arrayOrdenado[0] === -1 ){
-            return 0;
-        }
-        par = arrayOrdenado.length / 2;
-        parPlus = par + 1;
-        resultadoPar =( par + parPlus) / 2;
-        return resultadoPar;
-    }else{
-        if(arrayOrdenado.length <= 1){
-            return +arrayOrdenado[0];
-        }
-        if(+arrayOrdenado[arrayOrdenado.length - 1] === 0 ||  +arrayOrdenado[0] === -1 ){
-            return 0;
-        }
-        impar = arrayOrdenado.length / 2;
-        impar = Math.ceil(impar);
-        return impar;
+    parPlus = +arrayOrdenado[parPosicion + 1];
+    resultadoPar = (par + parPlus) / 2;
+
+    return resultadoPar;
+  } else {
+    if (arrayOrdenado.length <= 1) {
+      return +arrayOrdenado[0];
     }
+    impar = arrayOrdenado.length;
 
-};
+    imparPosicion = Math.floor(impar / 2);
 
-console.log(findMedianSortedArrays([0,0,0,0,0],[-1,0,0,0,0,0,1]));
-
-
-const numeros = [1,3,0];
-let respuesta = numeros.sort(function(a, b){return a - b});
+    return +arrayOrdenado[imparPosicion];
+  }
+}
